692d3053f952616c7a84e0a905381697f22c266f
Evgeny <ges1987@list.ru>
2021-12-11 14:27:25 +0700

    4-3 фильтр настроен окончательно правильно, добавлена дата в периоде, в интервале, все как по заданию 4-2 сделан исправный вывод в соответствии с фильтром, выводятся все статьи с нужными параметрами 4-1 добавлен пиганитор добавлены новые статьи добавлен постраничный вывод (одна статья на каждой странице)

--- newapp/filters.py
+++ newapp/filters.py
@@ -1,8 +1,9 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, CharFilter, RangeFilter, DateFromToRangeFilter
 from .models import Post
 
 
 class NewsFilter(FilterSet):
+    dateCreation = DateFromToRangeFilter()
     class Meta:
         model = Post
         fields = ('author', 'category', 'dateCreation')
