833691f80f247735e5bc801bbea3507f83d010eb
Evgeny <ges1987@list.ru>
2021-12-12 13:33:28 +0700

    4-13 готовый вариант все работает, реализованы функции (добавить новость, удалить, редактировать, поиск по фильтрам - дата (по ренджу), по автору, по категории, можно добавить по всем полям 4-12 осталось сделать страницу поиска, и пагинатор перенести на нее 4-11 Все исправно полностью, осталось добавить отдельную страницу для поиска 4-10 найдена ошибка ссылок редактирования и удаления, проблема в них, пока не разобрался 4-9 Большую часть ошибок исправил, хотел поменять путаницу в названия при слиянии неск проектов воедино, получилось так, что стала вылезать ошибка Reverse for 'news_update' not found. 'news_update' is not a valid view function or pattern name. никак не лечется 4-8 разбил по страницам, запустил кое-как, работает только небольшая честь, нужно исправить функции редактир, удаления, добавления, короч почти все, но главное выводится пока что главная страница 4-7 добавлена исправная форма для ввода новых статей, все добавляется, все отлично работает 4-6 никак не работает форма пост, ее  тупо нет на странице, и кнопка также не работает, так как формы нет, нет и данных 4-5 самопальная форма создания товаров не работает, вызывает ошибку, что поле автор не является полем Пост 4-4 дополнительный коммит до начала внедрения новых страниц, и изменения урлов, добавлен также на гит 4-3 фильтр настроен окончательно правильно, добавлена дата в периоде, в интервале, все как по заданию 4-2 сделан исправный вывод в соответствии с фильтром, выводятся все статьи с нужными параметрами 4-1 добавлен пиганитор добавлены новые статьи добавлен постраничный вывод (одна статья на каждой странице)

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import PostsList, NewsDetailView, NewsAddView, NewsEditView, NewsDeleteView, NewsSearchView
+from .views import NewsList, NewsDetailView, NewsAddView, NewsEditView, NewsDeleteView, NewsSearch #, PostsList
 
 
 urlpatterns = [
-    path('', PostsList.as_view()),
+    path('', NewsList.as_view()),
+#    path('', PostsList.as_view()),
     path('<int:pk>/', NewsDetailView.as_view(), name='news_detail'),  # Ссылка на детали новости
     path('add/', NewsAddView.as_view(), name='news_add'),  # Ссылка на создание новости
     path('edit/<int:pk>', NewsEditView.as_view(), name='news_edit'),
     path('delete/<int:pk>', NewsDeleteView.as_view(), name='news_delete'),
-    path('search/', NewsSearchView.as_view(), name='news_search'),
+    path('search/', NewsSearch.as_view(), name='news_search'),
 ]
