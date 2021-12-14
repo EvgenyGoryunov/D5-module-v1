a2a3078fca1836dccd501d05eee7245f0ce38a0f
Evgeny_Goryunov <ges987@list.ru>
2021-12-14 22:19:50 +0700

    5-5 перенос завершился успешно, сайт работает, ошибок 0 5-4 глобальная перестановка папок 5-3 создано 2 приложения sing and protect 5-2 страницу приветствия начал делать 5-1 исходник, скаченный с гита, и все

--- news_app/project/urls.py
+++ news_app/project/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from newapp.views import ProtectedView
+
 
 urlpatterns = [
-    path('', ProtectedView.as_view(), name='protected_page'),
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('news/', include('newapp.urls')),
