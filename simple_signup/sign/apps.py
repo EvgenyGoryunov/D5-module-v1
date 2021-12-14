d5b0df735859ee358a1b39d89a126ef6015830e3
Evgeny_Goryunov <ges987@list.ru>
2021-12-14 23:58:55 +0700

    5-7 входит и выходит, как у ослика Иа 5-6 созданы и почти настроены приложухи для пользователей 5-5 перенос завершился успешно, сайт работает, ошибок 0 5-4 глобальная перестановка папок 5-3 создано 2 приложения sing and protect 5-2 страницу приветствия начал делать 5-1 исходник, скаченный с гита, и все

--- simple_signup/sign/apps.py
+++ simple_signup/sign/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sing'
+    name = 'sign'
--- simple_signup/sign/urls.py
+++ simple_signup/sign/urls.py
@@ -2,12 +2,10 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 urlpatterns = [
-
 # В первых строках мы импортируем класс-представление для аутентификации LoginView
     path('login/', LoginView.as_view(template_name='sign/login.html'), name='login'),
-
 # а заодно и для выхода из системы — Logout
-# При выходе с сайта (вспоминаем кнопку, которую мы создали раньше в шаблоне index.html) Django
+# При выходе с сайта (кнопку, которую мы создали раньше в шаблоне index.html) Django
 # перенаправит пользователя на страницу, указанную в параметре template_name класса LogoutView
     path('logout/', LogoutView.as_view(template_name='sign/logout.html'), name='logout'),
 ]
