88b8e3a11c47853d64fd7f4045c6e62a8e8b3be3
Evgeny_Goryunov <ges987@list.ru>
2021-12-17 14:50:18 +0700

    5-23 финиш ver2 модуля Д5 (убраны лишние файлы) 5-22 финиш (п.6 - форма регистрации через google-аккаунт временно отключена, (в settings # LOGIN_URL = '/accounts/login/'), и включена другая форма LOGIN_URL = 'sign/login/', более удобна для работы с проектом) 5-21 добавлены описания нек функциям 5-20 промежуточный 5-19 модуль закончен, эксперименты с группами под наш проект 5-18 гугл авторизуется нормально, проблем нет 5-17 до гугла дошел, работает Ок 5-16 соединение файлов с моим проектом 5-15 начало выполнения практики Д5.4 5-14 М5.6 финиш 5-13 М5.5 финиш (кнопка премиум работает) 5-12 М5.5 Добавление в группы при регистрации 5-11 Д4 финишный вариант 5-10 проверочный 5-9 все работает, дошел до регистр посредст провайд 5-8 ошибки, не находит шаблоны после подключения allauth 5-7 входит и выходит, как у ослика Иа 5-6 созданы и почти настроены приложухи для пользователей 5-5 перенос завершился успешно, сайт работает, ошибок 0 5-4 глобальная перестановка папок 5-3 создано 2 приложения sing and protect 5-2 страницу приветствия начал делать 5-1 исходник, скаченный с гита, и все

--- simple_signup/protect/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import IndexView
-
-# перенаправляемся на единственное представление IndexView, которое описано в соответствующем файле views.py
-urlpatterns = [
-    path('', IndexView.as_view()),
-]
-
