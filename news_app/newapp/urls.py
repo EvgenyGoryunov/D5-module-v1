f2774a23f12292795d113027e67d7235d77ca0ec
Evgeny_Goryunov <ges987@list.ru>
2021-12-17 14:45:55 +0700

    5-22 финиш (п.6 - форма регистрации через google-аккаунт временно отключена, (в settings # LOGIN_URL = '/accounts/login/'), и включена другая форма LOGIN_URL = 'sign/login/', более удобна для работы с проектом) 5-21 добавлены описания нек функциям 5-20 промежуточный 5-19 модуль закончен, эксперименты с группами под наш проект 5-18 гугл авторизуется нормально, проблем нет 5-17 до гугла дошел, работает Ок 5-16 соединение файлов с моим проектом 5-15 начало выполнения практики Д5.4 5-14 М5.6 финиш 5-13 М5.5 финиш (кнопка премиум работает) 5-12 М5.5 Добавление в группы при регистрации 5-11 Д4 финишный вариант 5-10 проверочный 5-9 все работает, дошел до регистр посредст провайд 5-8 ошибки, не находит шаблоны после подключения allauth 5-7 входит и выходит, как у ослика Иа 5-6 созданы и почти настроены приложухи для пользователей 5-5 перенос завершился успешно, сайт работает, ошибок 0 5-4 глобальная перестановка папок 5-3 создано 2 приложения sing and protect 5-2 страницу приветствия начал делать 5-1 исходник, скаченный с гита, и все

--- news_app/newapp/urls.py
+++ news_app/newapp/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
-from .views import NewsList, NewsDetailView, NewsAddView, NewsEditView, NewsDeleteView, NewsSearch
+from .views import NewsList, NewsDetailView, NewsSearch, AddNews, ChangeNews, DeleteNews
 
 
 urlpatterns = [
     path('', NewsList.as_view()),
     path('<int:pk>/', NewsDetailView.as_view(), name='news_detail'),  # Ссылка на детали новости
-    path('add/', NewsAddView.as_view(), name='news_add'),  # Ссылка на создание новости
-    path('edit/<int:pk>', NewsEditView.as_view(), name='news_edit'),
-    path('delete/<int:pk>', NewsDeleteView.as_view(), name='news_delete'),
     path('search/', NewsSearch.as_view(), name='news_search'),
-    # path('userr/', include('project.urls'), name='userr')
+
+    # модуль Д5
+    # добавлено новое представление во view с ограничением прав доступа, изначально ограничиваем права в админ панели,
+    # там нужно из огромного списка выбрать наше приложения (newapp) и варианты ограничения, такие как
+    # Can add post например (выбрал еще Can change post, Can delete post)
+    path('add/', AddNews.as_view(), name='news_add'),
+    path('edit/<int:pk>', ChangeNews.as_view(), name='news_edit'),
+    path('delete/<int:pk>', DeleteNews.as_view(), name='news_delete'),
 ]
--- news_app/newapp/views.py
+++ news_app/newapp/views.py
@@ -1,34 +1,48 @@
-from .models import Author, Category, Post, Comment
-from django.views.generic import ListView, UpdateView, CreateView, DetailView, DeleteView  # импортируем необходимые дженерики
+from django.contrib.auth.mixins import PermissionRequiredMixin  # модуль Д5, чтоб ограничить права доступа
+# импортируем необходимые дженерики модуль Д4
+from django.views.generic import ListView, UpdateView, CreateView, DetailView, DeleteView
+
 from .filters import NewsFilter  # импортируем написанный нами фильтр (с файла filters.py)
 from .forms import NewsForm
+from .models import Post
 
 
 class NewsList(ListView):
     model = Post  # указываем модель, объекты которой мы будем выводить
-    template_name = 'news_list.html'  # указываем имя шаблона, в котором будет лежать HTML, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
-    context_object_name = 'posts'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+    # указываем имя шаблона, в котором будет лежать HTML, в котором будут все инструкции о том, как именно
+    # пользователю должны вывестись наши объекты
+    template_name = 'news_list.html'
+    # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку
+    # объектов через HTML-шаблон
+    context_object_name = 'posts'
     ordering = ['-dateCreation']
     paginate_by = 5
 
-    def get_context_data(self, **kwargs):  # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса
+    # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['filter'] = NewsFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
+        # вписываем наш фильтр в контекст
+        context['filter'] = NewsFilter(self.request.GET, queryset=self.get_queryset())
         return context
 
 
 # дженерик для поиска постов
 class NewsSearch(ListView):
     model = Post  # указываем модель, объекты которой мы будем выводить
-    template_name = 'news_search.html'  # указываем имя шаблона, в котором будет лежать HTML, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
-    context_object_name = 'posts'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+    # указываем имя шаблона, в котором будет лежать HTML, в котором будут все инструкции о том, как именно
+    # пользователю должны вывестись наши объекты
+    template_name = 'news_search.html'
+    # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку
+    # объектов через HTML-шаблон
+    context_object_name = 'posts'
     ordering = ['-dateCreation']
     paginate_by = 5
 
-
-    def get_context_data(self, **kwargs):  # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса
+    # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['filter'] = NewsFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
+        # вписываем наш фильтр в контекст
+        context['filter'] = NewsFilter(self.request.GET, queryset=self.get_queryset())
         return context
 
 
@@ -38,7 +52,7 @@ class NewsDetailView(DetailView):
     queryset = Post.objects.all()
 
 
-# дженерик для создания объекта. Надо указать только имя шаблона и класс формы, который мы написали в прошлом юните. Остальное он сделает за вас
+# дженерик для создания объекта. Надо указать только имя шаблона и класс формы
 class NewsAddView(CreateView):
     template_name = 'news_add.html'
     form_class = NewsForm
@@ -51,7 +65,8 @@ class NewsEditView(UpdateView):
     form_class = NewsForm
     success_url = '/news/'  # после редактирования нашей статьи нас будет перебрасывать по указанному адресу
 
-    # метод get_object мы используем вместо queryset, чтобы получить информацию об объекте, который мы собираемся редактировать
+    # метод get_object мы используем вместо queryset, чтобы получить информацию об объекте, который мы собираемся
+    # редактировать
     def get_object(self, **kwargs):
         id = self.kwargs.get('pk')
         return Post.objects.get(pk=id)
@@ -63,3 +78,21 @@ class NewsDeleteView(DeleteView):
     queryset = Post.objects.all()
     success_url = '/news/'  # после удаления нашей статьи нас будет перебрасывать по указанному адресу
 
+
+# Модуль Д5 - Ограничения прав доступа
+# через запятую указываем какие права хотим ограничить, предварительно в админ панели создали данные ограничения,
+# а в данном месте мы накладываем ограничения конкретно на представление, то есть выводы страничек сайта, если
+# пользователь не входит в нужную группу, ему летает страница с ошибкой 403 (страница недоступна)
+# Существует определенное соглашение для именования разрешений: <app>.<action>_<model>, пример 'newapp.add_post'
+# После того, как мы написали наши ограничения, нужно в urls изменить выводы преставлений,указав на новые классы (ниже)
+
+class AddNews(PermissionRequiredMixin, NewsAddView):
+    permission_required = ('newapp.add_post',)
+
+
+class ChangeNews(PermissionRequiredMixin, NewsEditView):
+    permission_required = ('newapp.change_post',)
+
+
+class DeleteNews(PermissionRequiredMixin, NewsDeleteView):
+    permission_required = ('newapp.delete_post',)
