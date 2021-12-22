5930fca48b811cb7d8172a6e50fae4c6e1642e79
Evgeny_Goryunov <ges987@list.ru>
2021-12-22 17:27:35 +0700

    6-3 rabotaet sohranenie v bazu, delau nastrojku atpravki pisem 6-2 nastroen vivod shablona s formoiy 6-1 sozdano prilozenie oppointments

--- news_app/appointments/urls.py
+++ news_app/appointments/urls.py
@@ -5,5 +5,9 @@ from .views import AppointmentView
 urlpatterns = [
 
     # модуль Д6 - отправка писем
-    path('', AppointmentView.as_view()),
+    # http://127.0.0.1:8000/appointment/
+    path('', AppointmentView.as_view(), name='make_appointment'),
+    path('/appointments:make_appointment/', AppointmentView.as_view()),
+    # path('', AppointmentView.as_view()),
+
 ]
